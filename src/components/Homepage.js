04141b93a16a06adcd758b8624a6d1409d137610
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-20 12:17:39 -0500

    put Homepage in components folder

---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import { Nav } from "./Nav";
+import Introduction from "./Introduction";
+import { Contact } from "./Contact";
+
+export default function Homepage() {
+  return (
+    <main>
+      <Nav />
+      <Introduction />
+      <Contact />
+    </main>
+  );
+};
