43f101b49af39df089fe09924b4745d8bdc96092
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-20 05:35:26 -0500

    finish styling

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,13 +7,13 @@ export default function Introduction() {
     <React.Fragment>
       <div className="profile-photo">
         <img src="https://drive.google.com/uc?export=view&id=1Ydd8gkGk_Su_N1txkEG2kpiMgbjceOvS" alt="profile-photo" />
-      </div>
-      <div className="intro-text">
         <p id="title">-Junior Full Stack Web Developer-</p>
-        <p id="first">An enthusiastic Full Stack Developer graduated from Lighthouse Lab.</p>
-        <p id="second">Skilled with JavaScript and React.js front-end developing. Also strong in Express.js backend building.</p>
-        <p id="third">A fast-learner and can be kept up to date with the new introduced contents. Indeed enjoys the programming process in web development and is willing to learn to become a competitive developer.</p>
       </div>
+        <div className="intro-text">
+          <p id="content">An enthusiastic Full Stack Developer graduated from Lighthouse Lab.</p>
+          <p id="content">Skilled with JavaScript and React.js front-end developing. Also strong in Express.js backend building.</p>
+          <p id="content">A fast-learner and can be kept up to date with the new introduced contents. Indeed enjoys the programming process in web development and is willing to learn to become a competitive developer.</p>
+        </div>
     </React.Fragment>
   );
 };
