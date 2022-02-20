c10b3ddb23c85db8d3106659ea514d33ea6d4f8e
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-20 05:34:29 -0500

    add contact me text and styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,22 @@ import React from "react";
 import { IconContext } from "react-icons";
 import { MdEmail } from "react-icons/md";
 import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
-import "./Contact.css";
+import "./Contact.scss";
 
 
 class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
-        <IconContext.Provider value={{ color: '#66ccff', size: '2em', className: 'contact-icons' }}>
-          <a href="mailto:chenbosi.bosi@gmail.com"><MdEmail /></a>
-          <a href="https://www.linkedin.com/in/bosi-chen-79657421b/" target="_blank"><BsLinkedin /></a>
-          <a href="https://github.com/BosiC0015" target="_blank"><BsGithub /></a>
-          <a href=""><BsTwitter /></a>
-        </IconContext.Provider>
+        <p className="contact-me">Contact me:</p>
+        <div className="contact-icons">
+          <IconContext.Provider value={{ color: '#66ccff', size: '2em', className: 'contact-icons' }}>
+            <a href="mailto:chenbosi.bosi@gmail.com"><MdEmail /></a>
+            <a href="https://www.linkedin.com/in/bosi-chen-79657421b/" target="_blank"><BsLinkedin /></a>
+            <a href="https://github.com/BosiC0015" target="_blank"><BsGithub /></a>
+            <a href=""><BsTwitter /></a>
+          </IconContext.Provider>
+        </div>
       </div>
     )
   }
