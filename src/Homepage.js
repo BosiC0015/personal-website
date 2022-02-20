98f7fec228084c428c0ae130910bc0e31fdc122d
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-20 00:47:43 -0500

    add the introduction part

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,8 +1,10 @@
 import { Contact } from "./components/Contact";
+import Introduction from "./components/Introduction";
 
 export default function Homepage() {
   return (
     <main>
+      <Introduction />
       <Contact />
     </main>
   );
