f3679869e06eff87c0effd5f3423a8e884404e27
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-19 23:15:49 -0500

    add contact-me part

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { MdEmail } from "react-icons/md";
+import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
+import "./Contact.scss";
+
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className="contact">
+        <IconContext.Provider value={{ color: '#66ccff', size: '2em', className: 'contact-icons' }}>
+          <a href="mailto:chenbosi.bosi@gmail.com"><MdEmail /></a>
+          <a href="https://www.linkedin.com/in/bosi-chen-79657421b/" target="_blank"><BsLinkedin /></a>
+          <a href="https://github.com/BosiC0015" target="_blank"><BsGithub /></a>
+          <a href=""><BsTwitter /></a>
+        </IconContext.Provider>
+      </div>
+    )
+  }
+}
+
+export { Contact };
