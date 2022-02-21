9060cce55d4e7f13429552a166552584dc262ba8
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-20 22:13:57 -0500

    create project page outline

--- src/projectpage/Projectpage.js
+++ src/projectpage/Projectpage.js
@@ -1,10 +1,26 @@
 import { Nav } from "../components/Nav";
+import { ProjectHead } from "./ProjectHead";
+import "./Projectpage.css";
 
 export default function ProjectPage() {
   return (
     <main>
       <Nav />
-      projects
+      <div className="projects">
+        <ProjectHead />
+        <div className="projects-container">
+          <section className="project-card">
+            <div className="project-title">
+              <h5>MLTD Calculator</h5>
+            </div>
+          </section>
+          <section className="project-card">
+            <div className="project-title">
+              <h5>My Tiny Journal</h5>
+            </div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 };
