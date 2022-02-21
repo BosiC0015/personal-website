fd01047848b0436425533497b170d2d312b6da38
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-21 16:01:46 -0500

    hide icons with temporary inactive links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { IconContext } from "react-icons";
 import { MdEmail } from "react-icons/md";
-import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
+import { BsLinkedin, BsGithub, BsTwitter, BsMedium } from "react-icons/bs";
 import "./Contact.scss";
 
 
@@ -9,13 +9,14 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact" id="contact">
-        <p className="contact-me">Contact me:</p>
+        <p className="contact-me">Find & Contact Me:</p>
         <div className="contact-icons">
           <IconContext.Provider value={{ color: '#66ccff', size: '2em', className: 'contact-icons' }}>
             <a href="mailto:chenbosi.bosi@gmail.com"><MdEmail /></a>
             <a href="https://www.linkedin.com/in/bosi-chen-79657421b/" target="_blank"><BsLinkedin /></a>
             <a href="https://github.com/BosiC0015" target="_blank"><BsGithub /></a>
-            <a href=""><BsTwitter /></a>
+            {/* <a href="https://twitter.com/BosiChen0_o"><BsTwitter /></a> */}
+            {/* <a href="https://medium.com/@chenbosi.bosi" target="_blank"><BsMedium /></a> */}
           </IconContext.Provider>
         </div>
       </div>
