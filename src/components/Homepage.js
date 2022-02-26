d489d55d42a6845aaabf30212e3e5f18ff11004e
Bosi Chen <chenbosi.bosi@gmail.com>
2022-02-26 04:06:57 -0500

    add stacks img

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,14 @@
 import { Nav } from "./Nav";
 import Introduction from "./Introduction";
 import { Contact } from "./Contact";
+import Stacks from "./Stacks";
 
 export default function Homepage() {
   return (
     <main>
       <Nav />
       <Introduction />
+      <Stacks />
       <Contact />
     </main>
   );
---
+++ src/components/Stacks.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Stacks.css";
+
+
+export default function Stacks() {
+  return (
+    <div>
+      <p className="explore">My Stacks</p>
+      <div>
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MDMQaRCt7KvD6gbN-6Fe0Y_5rw-sdR_e" alt="js" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1LuC9FqQtI5apXHIPUmsXZ2R1ohMCeFMT" alt="python" />
+      </div>
+      <div>
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MGvn5vfXONSYBkV4CXE4iSJkDO8EXiXc" alt="node" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MfxzExsy3gbt6xH3Y_CN6R63Hc4qyL49" alt="express" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MgnQXlUMqAxEeYOZ6bLoIUDs3c6xSw7P" alt="postgres" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MwIo42zeCy1DaGmhZlQZ9b6wWsuK5skU" alt="jquery" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1Mkx0xG7oTTOSn4CMSIeLgI3z-HD5BFIP" alt="ejs" />
+      </div>
+      <div>
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MK4tg91CV7owfy8mwou-NSLpLJs_p68L" alt="react" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MS_vIXrac-LZlVOg4PTBZz9jAoc9Orfm" alt="html" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MdtSfROQsgumyXMHQx7HKhDmVRJ6igix" alt="css" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1ddZ7-57SOZaH4xQ6s7w29FhPw6WNNeUl" alt="scss" />
+      </div>
+      <div>
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MpX5EEe84wNTW4j56IdReOV_7lsfh500" alt="ruby" />
+        <img className="stack-img" src="https://drive.google.com/uc?export=view&id=1MtZphWX-BOVltJ9l0b1EmZOkoTt9BCHL" alt="ruby on rails" />
+      </div>
+    </div>
+  );
+};
